cebf5940be4d0d9053bf04c52d8d15eb9c78a14c
mulchus <mulch@list.ru>
2023-03-29 23:18:17 +0300

    Create HTML page with books.

---
+++ render_website.py
@@ -0,0 +1,35 @@
+import json
+import collections
+
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from environs import Env
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from pathlib import Path
+
+
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('template.html')
+
+    books_path = Path.joinpath(Path.cwd().parents[0], 'Book-parser/', 'about_books.json')
+    with open(books_path, 'r', encoding='utf-8') as json_file:
+        books = json.load(json_file)
+
+    books_set = collections.defaultdict(list)
+    rendered_page = template.render(
+        books=books
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    # server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    # server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
